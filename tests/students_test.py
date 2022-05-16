695a4d179726a72835716765733906cdc97c8b74
sumanth-fyle <99784932+sumanth-fyle@users.noreply.github.com>
2022-05-16 15:11:17 +0530

    WIP

--- tests/students_test.py
+++ tests/students_test.py
@@ -57,3 +57,17 @@ def test_submit_assignment_student_1(client, h_student_1):
     assert data['student_id'] == 1
     assert data['state'] == 'SUBMITTED'
     assert data['teacher_id'] == 2
+
+
+def test_assingment_resubmitt_error(client, h_student_1):
+    response = client.post(
+        '/student/assignments/submit',
+        headers=h_student_1,
+        json={
+            'id': 2,
+            'teacher_id': 2
+        })
+    error_response = response.json
+    assert response.status_code == 400
+    assert error_response['error'] == 'FyleError'
+    assert error_response["message"] == 'only a draft assignment can be submitted'
