353a369005cafc7f4535a9a53b722fd839a065f3
horizenight <kshitijroodkee1@gmail.com>
2023-04-06 01:59:57 +0530

    added more teachers test

--- tests/teachers_test.py
+++ tests/teachers_test.py
@@ -1,3 +1,4 @@
+
 def test_get_assignments_teacher_1(client, h_teacher_1):
     response = client.get(
         '/teacher/assignments',
@@ -26,6 +27,31 @@ def test_get_assignments_teacher_2(client, h_teacher_2):
         assert assignment['state'] == 'SUBMITTED'
 
 
+
+
+def test_grade_assignment_successful(client, h_teacher_1):
+    """
+    success case: grade a submitted assignment with valid grade
+    """
+    response = client.post(
+        '/teacher/assignments/grade',
+        headers=h_teacher_1,
+        json={
+            "id": 1,
+            "grade": "A"
+        }
+    )
+
+    assert response.status_code == 200
+    data = response.json
+
+    assert data['data']['id'] == 1
+    assert data['data']['grade'] == "A"
+    assert data['data']['state'] == "GRADED"
+  
+
+
+
 def test_grade_assignment_cross(client, h_teacher_2):
     """
     failure case: assignment 1 was submitted to teacher 1 and not teacher 2
@@ -39,10 +65,11 @@ def test_grade_assignment_cross(client, h_teacher_2):
         }
     )
 
-    assert response.status_code == 400
+    assert response.status_code == 403
     data = response.json
 
     assert data['error'] == 'FyleError'
+    assert data['message'] == 'Only the assigned teacher can grade this assignment'
 
 
 def test_grade_assignment_bad_grade(client, h_teacher_1):
@@ -62,6 +89,7 @@ def test_grade_assignment_bad_grade(client, h_teacher_1):
     data = response.json
 
     assert data['error'] == 'ValidationError'
+    assert data['message'] == {'grade': ['Invalid enum member AB']}
 
 
 def test_grade_assignment_bad_assignment(client, h_teacher_1):
@@ -81,6 +109,7 @@ def test_grade_assignment_bad_assignment(client, h_teacher_1):
     data = response.json
 
     assert data['error'] == 'FyleError'
+    assert data['message'] == 'No assignment with this id was found'
 
 
 def test_grade_assignment_draft_assignment(client, h_teacher_1):
@@ -96,7 +125,33 @@ def test_grade_assignment_draft_assignment(client, h_teacher_1):
         }
     )
 
-    assert response.status_code == 400
+    assert response.status_code == 403
     data = response.json
 
     assert data['error'] == 'FyleError'
+    assert data['message'] == 'Only the assigned teacher can grade this assignment'
+
+
+
+
+def test_grade_assignment_graded_assignment(client, h_teacher_1):
+    """
+    failure case: cannot grade an assignment that has already been graded
+    """
+    response = client.post(
+        '/teacher/assignments/grade',
+        headers=h_teacher_1,
+        json={
+            "id": 3,
+            "grade": "B"
+        }
+    )
+
+    assert response.status_code == 403
+    data = response.json
+
+    assert data['error'] == 'FyleError'
+    assert data['message'] == 'Only the assigned teacher can grade this assignment'
+
+
+
