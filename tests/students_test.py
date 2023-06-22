766dbfdb109eb1cd27020db9b4bd4c8a876ed442
Vikas Prasad <vikasprasad.prasad@gmail.com>
2023-06-22 13:11:56 +0530

    fix typo

--- tests/students_test.py
+++ tests/students_test.py
@@ -59,7 +59,7 @@ def test_submit_assignment_student_1(client, h_student_1):
     assert data['teacher_id'] == 2
 
 
-def test_assingment_resubmitt_error(client, h_student_1):
+def test_assignment_resubmit_error(client, h_student_1):
     response = client.post(
         '/student/assignments/submit',
         headers=h_student_1,
