7260da7ed8c6e7cf57dd0cb735183d6b83d5d11e
Vasudeo Gaichor <vasudeogaichor@gmail.com>
2024-01-15 17:37:31 +0530

    fix: bug fix for teachers_test.py

--- core/models/assignments.py
+++ core/models/assignments.py
@@ -80,6 +80,10 @@ class Assignment(db.Model):
         assignment = Assignment.get_by_id(_id)
         assertions.assert_found(assignment, 'No assignment with this id was found')
         assertions.assert_valid(grade is not None, 'assignment with empty grade cannot be graded')
+        assertions.assert_valid(auth_principal.teacher_id == assignment.teacher_id,
+                                'this assignment was submitted to other teacher')
+        assertions.assert_valid(assignment.state == AssignmentStateEnum.SUBMITTED,
+                                    'only a submitted assignment can be graded')
 
         assignment.grade = grade
         assignment.state = AssignmentStateEnum.GRADED
