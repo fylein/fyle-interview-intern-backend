8f823b389f2ebad364bb2f72b151e3f20dd24cbf
Ayush Jaipuriyar <ayushjaipuriyar21@gmail.com>
2024-01-22 08:42:15 +0000

    add: GET /principal/assignments

---
+++ core/apis/assignments/principal.py
@@ -0,0 +1,20 @@
+from flask import Blueprint
+from core.apis import decorators
+from core.models.assignments import Assignment
+from .schema import AssignmentSchema, AssignmentSubmitSchema
+from core.apis.responses import APIResponse
+
+principal_assignments_resources = Blueprint('principal_assignments_resources', __name__)
+
+
+@principal_assignments_resources.route(
+    "/assignments", methods=["GET"], strict_slashes=False
+)
+@decorators.authenticate_principal
+def list_assignments(p):
+    """Returns list of assignments"""
+    principal_assignments = Assignment.get_assignments_by_principal(p.principal_id)
+    principal_assignments_dump = AssignmentSchema().dump(
+        principal_assignments, many=True
+    )
+    return APIResponse.respond(data=principal_assignments_dump)
--- core/server.py
+++ core/server.py
@@ -1,7 +1,7 @@
 from flask import jsonify
 from marshmallow.exceptions import ValidationError
 from core import app
-from core.apis.assignments import student_assignments_resources, teacher_assignments_resources
+from core.apis.assignments import student_assignments_resources, teacher_assignments_resources,principal_assignments_resources
 from core.libs import helpers
 from core.libs.exceptions import FyleError
 from werkzeug.exceptions import HTTPException
@@ -10,6 +10,7 @@ from sqlalchemy.exc import IntegrityError
 
 app.register_blueprint(student_assignments_resources, url_prefix='/student')
 app.register_blueprint(teacher_assignments_resources, url_prefix='/teacher')
+app.register_blueprint(principal_assignments_resources, url_prefix='/principal')
 
 
 @app.route('/')
