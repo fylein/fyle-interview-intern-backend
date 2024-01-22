f382a719f35845ee5f0ee977c1e129db3fdaecb7
Ayush Jaipuriyar <ayushjaipuriyar21@gmail.com>
2024-01-22 13:43:11 +0000

    Fix: missing imports

--- core/apis/decorators.py
+++ core/apis/decorators.py
@@ -2,6 +2,9 @@ import json
 from flask import request
 from core.libs import assertions
 from functools import wraps
+from core.models.students import Student
+from core.models.teachers import Teacher
+from core.models.principals import Principal
 
 
 class AuthPrincipal:
@@ -17,36 +20,50 @@ def accept_payload(func):
     def wrapper(*args, **kwargs):
         incoming_payload = request.json
         return func(incoming_payload, *args, **kwargs)
+
     return wrapper
 
 
 def authenticate_principal(func):
     @wraps(func)
     def wrapper(*args, **kwargs):
-        p_str = request.headers.get('X-Principal')
-        assertions.assert_auth(p_str is not None, 'principal not found')
+        p_str = request.headers.get("X-Principal")
+        assertions.assert_auth(p_str is not None, "principal not found")
         p_dict = json.loads(p_str)
         p = AuthPrincipal(
-            user_id=p_dict['user_id'],
-            student_id=p_dict.get('student_id'),
-            teacher_id=p_dict.get('teacher_id'),
-            principal_id=p_dict.get('principal_id')
+            user_id=p_dict["user_id"],
+            student_id=p_dict.get("student_id"),
+            teacher_id=p_dict.get("teacher_id"),
+            principal_id=p_dict.get("principal_id"),
         )
 
-        if request.path.startswith('/student'):
-            assertions.assert_true(p.student_id is not None, 'requester should be a student')
+        if request.path.startswith("/student"):
+            assertions.assert_true(
+                p.student_id is not None, "requester should be a student"
+            )
             student = Student.get_by_id(p.student_id)
-            assertions.assert_true(p.user_id == student.user_id, 'authentication failed')
-        elif request.path.startswith('/teacher'):
-            assertions.assert_true(p.teacher_id is not None, 'requester should be a teacher')
+            assertions.assert_true(
+                p.user_id == student.user_id, "authentication failed"
+            )
+        elif request.path.startswith("/teacher"):
+            assertions.assert_true(
+                p.teacher_id is not None, "requester should be a teacher"
+            )
             teacher = Teacher.get_by_id(p.teacher_id)
-            assertions.assert_true(p.user_id == teacher.user_id, 'authentication failed')
-        elif request.path.startswith('/principal'):
-            assertions.assert_true(p.principal_id is not None, 'requester should be a principal')
+            assertions.assert_true(
+                p.user_id == teacher.user_id, "authentication failed"
+            )
+        elif request.path.startswith("/principal"):
+            assertions.assert_true(
+                p.principal_id is not None, "requester should be a principal"
+            )
             principal = Principal.get_by_id(p.principal_id)
-            assertions.assert_true(p.user_id == principal.user_id, 'authentication failed')
+            assertions.assert_true(
+                p.user_id == principal.user_id, "authentication failed"
+            )
         else:
-            assertions.assert_found(None, 'No such api')
+            assertions.assert_found(None, "No such api")
 
         return func(p, *args, **kwargs)
+
     return wrapper
