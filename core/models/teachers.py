01e6f25075ceddf01e39ef96668d08d158bcc009
Ayush Jaipuriyar <ayushjaipuriyar21@gmail.com>
2024-01-22 13:50:13 +0000

    Fix: methods does not exist

--- core/models/teachers.py
+++ core/models/teachers.py
@@ -3,15 +3,31 @@ from core.libs import helpers
 
 
 class Teacher(db.Model):
-    __tablename__ = 'teachers'
-    id = db.Column(db.Integer, db.Sequence('teachers_id_seq'), primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-    created_at = db.Column(db.TIMESTAMP(timezone=True), default=helpers.get_utc_now, nullable=False)
-    updated_at = db.Column(db.TIMESTAMP(timezone=True), default=helpers.get_utc_now, nullable=False, onupdate=helpers.get_utc_now)
+    __tablename__ = "teachers"
+    id = db.Column(db.Integer, db.Sequence("teachers_id_seq"), primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    created_at = db.Column(
+        db.TIMESTAMP(timezone=True), default=helpers.get_utc_now, nullable=False
+    )
+    updated_at = db.Column(
+        db.TIMESTAMP(timezone=True),
+        default=helpers.get_utc_now,
+        nullable=False,
+        onupdate=helpers.get_utc_now,
+    )
 
     def __repr__(self):
-        return '<Teacher %r>' % self.id
-    
+        return "<Teacher %r>" % self.id
+
+    @classmethod
+    def get_by_id(cls, _id):
+        return cls.filter(cls.id == _id).first()
+
+    @classmethod
+    def filter(cls, *criterion):
+        db_query = db.session.query(cls)
+        return db_query.filter(*criterion)
+
     @classmethod
     def get_list_of_teachers(cls):
         teachers = cls.query.all()
