9d18d73032e16f471473fdf75bd210fc84097110
Ayush Jaipuriyar <ayushjaipuriyar21@gmail.com>
2024-01-22 13:34:59 +0000

    fix: tests

--- tests/principals_test.py
+++ tests/principals_test.py
@@ -2,16 +2,26 @@ from core.models.assignments import AssignmentStateEnum, GradeEnum
 
 
 def test_get_assignments(client, h_principal):
-    response = client.get(
-        '/principal/assignments',
-        headers=h_principal
-    )
+    response = client.get("/principal/assignments", headers=h_principal)
 
     assert response.status_code == 200
 
-    data = response.json['data']
+    data = response.json["data"]
     for assignment in data:
-        assert assignment['state'] in [AssignmentStateEnum.SUBMITTED, AssignmentStateEnum.GRADED]
+        assert assignment["state"] in [
+            AssignmentStateEnum.SUBMITTED,
+            AssignmentStateEnum.GRADED,
+        ]
+
+
+def test_get_teachers_list(client, h_principal):
+    response = client.get("/principal/teachers", headers=h_principal)
+
+    assert response.status_code == 200
+
+    data = response.json["data"]
+    for teachers in data:
+        assert teachers["id"] in [1, 2]
 
 
 def test_grade_assignment_draft_assignment(client, h_principal):
@@ -19,12 +29,9 @@ def test_grade_assignment_draft_assignment(client, h_principal):
     failure case: If an assignment is in Draft state, it cannot be graded by principal
     """
     response = client.post(
-        '/principal/assignments/grade',
-        json={
-            'id': 5,
-            'grade': GradeEnum.A.value
-        },
-        headers=h_principal
+        "/principal/assignments/grade",
+        json={"id": 5, "grade": GradeEnum.A.value},
+        headers=h_principal,
     )
 
     assert response.status_code == 400
@@ -32,31 +39,25 @@ def test_grade_assignment_draft_assignment(client, h_principal):
 
 def test_grade_assignment(client, h_principal):
     response = client.post(
-        '/principal/assignments/grade',
-        json={
-            'id': 4,
-            'grade': GradeEnum.C.value
-        },
-        headers=h_principal
+        "/principal/assignments/grade",
+        json={"id": 4, "grade": GradeEnum.C.value},
+        headers=h_principal,
     )
 
     assert response.status_code == 200
 
-    assert response.json['data']['state'] == AssignmentStateEnum.GRADED.value
-    assert response.json['data']['grade'] == GradeEnum.C
+    assert response.json["data"]["state"] == AssignmentStateEnum.GRADED.value
+    assert response.json["data"]["grade"] == GradeEnum.C
 
 
 def test_regrade_assignment(client, h_principal):
     response = client.post(
-        '/principal/assignments/grade',
-        json={
-            'id': 4,
-            'grade': GradeEnum.B.value
-        },
-        headers=h_principal
+        "/principal/assignments/grade",
+        json={"id": 4, "grade": GradeEnum.B.value},
+        headers=h_principal,
     )
 
     assert response.status_code == 200
 
-    assert response.json['data']['state'] == AssignmentStateEnum.GRADED.value
-    assert response.json['data']['grade'] == GradeEnum.B
+    assert response.json["data"]["state"] == AssignmentStateEnum.GRADED.value
+    assert response.json["data"]["grade"] == GradeEnum.B
--- tests/students_test.py
+++ tests/students_test.py
@@ -1,27 +1,21 @@
 def test_get_assignments_student_1(client, h_student_1):
-    response = client.get(
-        '/student/assignments',
-        headers=h_student_1
-    )
+    response = client.get("/student/assignments", headers=h_student_1)
 
     assert response.status_code == 200
 
-    data = response.json['data']
+    data = response.json["data"]
     for assignment in data:
-        assert assignment['student_id'] == 1
+        assert assignment["student_id"] == 1
 
 
 def test_get_assignments_student_2(client, h_student_2):
-    response = client.get(
-        '/student/assignments',
-        headers=h_student_2
-    )
+    response = client.get("/student/assignments", headers=h_student_2)
 
     assert response.status_code == 200
 
-    data = response.json['data']
+    data = response.json["data"]
     for assignment in data:
-        assert assignment['student_id'] == 2
+        assert assignment["student_id"] == 2
 
 
 def test_post_assignment_null_content(client, h_student_1):
@@ -30,59 +24,64 @@ def test_post_assignment_null_content(client, h_student_1):
     """
 
     response = client.post(
-        '/student/assignments',
-        headers=h_student_1,
-        json={
-            'content': None
-        })
+        "/student/assignments", headers=h_student_1, json={"content": None}
+    )
 
     assert response.status_code == 400
 
 
 def test_post_assignment_student_1(client, h_student_1):
-    content = 'ABCD TESTPOST'
+    content = "ABCD TESTPOST"
 
     response = client.post(
-        '/student/assignments',
-        headers=h_student_1,
-        json={
-            'content': content
-        })
+        "/student/assignments", headers=h_student_1, json={"content": content}
+    )
 
     assert response.status_code == 200
 
-    data = response.json['data']
-    assert data['content'] == content
-    assert data['state'] == 'DRAFT'
-    assert data['teacher_id'] is None
+    data = response.json["data"]
+    assert data["content"] == content
+    assert data["state"] == "DRAFT"
+    assert data["teacher_id"] is None
+
+
+def test_edit_assignment_student_1(client, h_student_1):
+    content = "edit assignment test "
+
+    response = client.post(
+        "/student/assignments", headers=h_student_1, json={"content": content, "id": 2}
+    )
+
+    assert response.status_code == 200
+
+    data = response.json["data"]
+    assert data["content"] == content
+    assert data["state"] == "DRAFT"
+    assert data["teacher_id"] is None
 
 
 def test_submit_assignment_student_1(client, h_student_1):
     response = client.post(
-        '/student/assignments/submit',
+        "/student/assignments/submit",
         headers=h_student_1,
-        json={
-            'id': 2,
-            'teacher_id': 2
-        })
+        json={"id": 2, "teacher_id": 2},
+    )
 
     assert response.status_code == 200
 
-    data = response.json['data']
-    assert data['student_id'] == 1
-    assert data['state'] == 'SUBMITTED'
-    assert data['teacher_id'] == 2
+    data = response.json["data"]
+    assert data["student_id"] == 1
+    assert data["state"] == "SUBMITTED"
+    assert data["teacher_id"] == 2
 
 
 def test_assignment_resubmit_error(client, h_student_1):
     response = client.post(
-        '/student/assignments/submit',
+        "/student/assignments/submit",
         headers=h_student_1,
-        json={
-            'id': 2,
-            'teacher_id': 2
-        })
+        json={"id": 2, "teacher_id": 2},
+    )
     error_response = response.json
     assert response.status_code == 400
-    assert error_response['error'] == 'FyleError'
-    assert error_response["message"] == 'Only a draft assignment can be submitted'
+    assert error_response["error"] == "FyleError"
+    assert error_response["message"] == "Only a draft assignment can be submitted"
