728bcd51b39014efe9a7bbbaab55e1373feb4511
fumaxin <kalpeshdhoundiyal@gmail.com>
2024-04-18 15:37:02 +0530

    bug fixes

--- core/models/assignments.py
+++ core/models/assignments.py
@@ -50,10 +50,12 @@ class Assignment(db.Model):
             assertions.assert_found(assignment, 'No assignment with this id was found')
             assertions.assert_valid(assignment.state == AssignmentStateEnum.DRAFT,
                                     'only assignment in draft state can be edited')
+            assertions.assert_valid(assignment.content not in ["", None], 'assignment with empty content cannot be edited')
 
             assignment.content = assignment_new.content
         else:
             assignment = assignment_new
+            assertions.assert_valid(assignment.content not in ["", None], 'assignment with empty content cannot be edited')
             db.session.add(assignment_new)
 
         db.session.flush()
@@ -65,7 +67,7 @@ class Assignment(db.Model):
         assertions.assert_found(assignment, 'No assignment with this id was found')
         assertions.assert_valid(assignment.student_id == auth_principal.student_id, 'This assignment belongs to some other student')
         assertions.assert_valid(assignment.content is not None, 'assignment with empty content cannot be submitted')
-        assertions.assert_valid(assignment.state == AssignmentStateEnum.DRAFT, 'assignment already submitted')
+        assertions.assert_valid(assignment.state == AssignmentStateEnum.DRAFT, 'only a draft assignment can be submitted')
 
         assignment.teacher_id = teacher_id
         assignment.state = AssignmentStateEnum.SUBMITTED
@@ -80,7 +82,7 @@ class Assignment(db.Model):
         assertions.assert_found(assignment, 'No assignment with this id was found')
         assertions.assert_valid(grade is not None, 'assignment with empty grade cannot be graded')
         if not auth_principal.principal_id:
-            assertions.assert_true(assignment.teacher_id == auth_principal.teacher_id, 'This assignment belongs to some other teacher')
+            assertions.assert_valid(assignment.teacher_id == auth_principal.teacher_id, 'This assignment belongs to some other teacher')
 
         assignment.grade = grade
         assignment.state = AssignmentStateEnum.GRADED
