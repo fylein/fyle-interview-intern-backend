e887417df3b892f2c9392e851364cd6dd42365aa
fumaxin <kalpeshdhoundiyal@gmail.com>
2024-04-18 16:50:10 +0530

    sql bug fixes

--- core/models/assignments.py
+++ core/models/assignments.py
@@ -81,6 +81,7 @@ class Assignment(db.Model):
         assignment = Assignment.get_by_id(_id)
         assertions.assert_found(assignment, 'No assignment with this id was found')
         assertions.assert_valid(grade is not None, 'assignment with empty grade cannot be graded')
+        assertions.assert_valid(assignment.state != AssignmentStateEnum.DRAFT, 'Draft assignment cannot be graded')
         if not auth_principal.principal_id:
             assertions.assert_valid(assignment.teacher_id == auth_principal.teacher_id, 'This assignment belongs to some other teacher')
 
