e0c6a3ea8d52992e9f3379bb59222a5741061082
Rajarshi Roy <royrajarshi0123@gmail.com>
2024-04-20 15:31:49 +0530

    added to increase test coverage

--- tests/teachers_test.py
+++ tests/teachers_test.py
@@ -1,28 +1,22 @@
 def test_get_assignments_teacher_1(client, h_teacher_1):
-    response = client.get(
-        '/teacher/assignments',
-        headers=h_teacher_1
-    )
+    response = client.get("/teacher/assignments", headers=h_teacher_1)
 
     assert response.status_code == 200
 
-    data = response.json['data']
+    data = response.json["data"]
     for assignment in data:
-        assert assignment['teacher_id'] == 1
+        assert assignment["teacher_id"] == 1
 
 
 def test_get_assignments_teacher_2(client, h_teacher_2):
-    response = client.get(
-        '/teacher/assignments',
-        headers=h_teacher_2
-    )
+    response = client.get("/teacher/assignments", headers=h_teacher_2)
 
     assert response.status_code == 200
 
-    data = response.json['data']
+    data = response.json["data"]
     for assignment in data:
-        assert assignment['teacher_id'] == 2
-        assert assignment['state'] in ['SUBMITTED', 'GRADED']
+        assert assignment["teacher_id"] == 2
+        assert assignment["state"] in ["SUBMITTED", "GRADED"]
 
 
 def test_grade_assignment_cross(client, h_teacher_2):
@@ -30,18 +24,13 @@ def test_grade_assignment_cross(client, h_teacher_2):
     failure case: assignment 1 was submitted to teacher 1 and not teacher 2
     """
     response = client.post(
-        '/teacher/assignments/grade',
-        headers=h_teacher_2,
-        json={
-            "id": 1,
-            "grade": "A"
-        }
+        "/teacher/assignments/grade", headers=h_teacher_2, json={"id": 1, "grade": "A"}
     )
 
     assert response.status_code == 400
     data = response.json
 
-    assert data['error'] == 'FyleError'
+    assert data["error"] == "FyleError"
 
 
 def test_grade_assignment_bad_grade(client, h_teacher_1):
@@ -49,18 +38,13 @@ def test_grade_assignment_bad_grade(client, h_teacher_1):
     failure case: API should allow only grades available in enum
     """
     response = client.post(
-        '/teacher/assignments/grade',
-        headers=h_teacher_1,
-        json={
-            "id": 1,
-            "grade": "AB"
-        }
+        "/teacher/assignments/grade", headers=h_teacher_1, json={"id": 1, "grade": "AB"}
     )
 
     assert response.status_code == 400
     data = response.json
 
-    assert data['error'] == 'ValidationError'
+    assert data["error"] == "ValidationError"
 
 
 def test_grade_assignment_bad_assignment(client, h_teacher_1):
@@ -68,18 +52,15 @@ def test_grade_assignment_bad_assignment(client, h_teacher_1):
     failure case: If an assignment does not exists check and throw 404
     """
     response = client.post(
-        '/teacher/assignments/grade',
+        "/teacher/assignments/grade",
         headers=h_teacher_1,
-        json={
-            "id": 100000,
-            "grade": "A"
-        }
+        json={"id": 100000, "grade": "A"},
     )
 
     assert response.status_code == 404
     data = response.json
 
-    assert data['error'] == 'FyleError'
+    assert data["error"] == "FyleError"
 
 
 def test_grade_assignment_draft_assignment(client, h_teacher_1):
@@ -87,15 +68,24 @@ def test_grade_assignment_draft_assignment(client, h_teacher_1):
     failure case: only a submitted assignment can be graded
     """
     response = client.post(
-        '/teacher/assignments/grade',
-        headers=h_teacher_1
-        , json={
-            "id": 2,
-            "grade": "A"
-        }
+        "/teacher/assignments/grade", headers=h_teacher_1, json={"id": 2, "grade": "A"}
     )
 
     assert response.status_code == 400
     data = response.json
 
-    assert data['error'] == 'FyleError'
+    assert data["error"] == "FyleError"
+
+
+""" Added to increase test coverage """
+
+
+def test_grade_assignment_cross(client, h_teacher_2):
+    """
+    I have chosen these specific parameters such that this fuction won't change the database
+    """
+    response = client.post(
+        "/teacher/assignments/grade", headers=h_teacher_2, json={"id": 4, "grade": "B"}
+    )
+
+    assert response.status_code == 200
