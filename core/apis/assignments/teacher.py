004bfa1610ff08bdff7c896b11d7d8d08bb82e1c
sumanth-fyle <99784932+sumanth-fyle@users.noreply.github.com>
2024-07-12 15:11:49 +0530

    Update Application.md

--- core/apis/assignments/teacher.py
+++ core/apis/assignments/teacher.py
@@ -3,6 +3,8 @@ from core import db
 from core.apis import decorators
 from core.apis.responses import APIResponse
 from core.models.assignments import Assignment
+from core.libs import assertions
+from ...libs.exceptions import FyleError
 
 from .schema import AssignmentSchema, AssignmentGradeSchema
 teacher_assignments_resources = Blueprint('teacher_assignments_resources', __name__)
@@ -12,7 +14,8 @@ teacher_assignments_resources = Blueprint('teacher_assignments_resources', __nam
 @decorators.authenticate_principal
 def list_assignments(p):
     """Returns list of assignments"""
-    teachers_assignments = Assignment.get_assignments_by_teacher()
+    # bug fixing: returning list on the basis of teacher_id
+    teachers_assignments = Assignment.get_assignments_by_teacher(p.teacher_id)
     teachers_assignments_dump = AssignmentSchema().dump(teachers_assignments, many=True)
     return APIResponse.respond(data=teachers_assignments_dump)
 
@@ -21,14 +24,21 @@ def list_assignments(p):
 @decorators.accept_payload
 @decorators.authenticate_principal
 def grade_assignment(p, incoming_payload):
-    """Grade an assignment"""
+    #Grade an assignment
+    if not incoming_payload:
+        assertions.assert_valid(None, "Payload is missing")
     grade_assignment_payload = AssignmentGradeSchema().load(incoming_payload)
-
-    graded_assignment = Assignment.mark_grade(
-        _id=grade_assignment_payload.id,
-        grade=grade_assignment_payload.grade,
-        auth_principal=p
-    )
+    assignment = Assignment.get_by_id(grade_assignment_payload.id)
+    if assignment is None:
+        raise FyleError(404, 'Assignment not found')
+    if p.teacher_id != assignment.teacher_id:
+        raise FyleError(400, 'Assignment submitted to another teacher')
+    else:
+        graded_assignment = Assignment.mark_grade(
+            _id=grade_assignment_payload.id,
+            grade=grade_assignment_payload.grade,
+            auth_principal=p
+        )
     db.session.commit()
     graded_assignment_dump = AssignmentSchema().dump(graded_assignment)
     return APIResponse.respond(data=graded_assignment_dump)
