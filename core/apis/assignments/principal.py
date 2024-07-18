f2d1717f4f4e4968fc46093d51293943e1d04476
Mahendra GL <mahendragl012@gmail.com>
2024-07-18 09:32:26 +0000

    Main

---
+++ core/apis/assignments/principal.py
@@ -0,0 +1,4 @@
+# Principal can view all the teachers
+# A principal can view all the assignments submitted and/or graded by teachers.
+# A principal can re-grade the assignments already graded by the teacher.
+from flask import Blueprint
