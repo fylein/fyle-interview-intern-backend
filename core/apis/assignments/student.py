f8a756e3c2fa0527a270b2087a9014013c03f7e8
Apoorvsh28 <apoorvsharma@Apoorvs-MacBook-Air.local>
2024-07-24 23:18:12 +0530

    Initial commit

--- core/apis/assignments/student.py
+++ core/apis/assignments/student.py
@@ -1,40 +1,19 @@
 from flask import Blueprint
+from flask import jsonify
 from core import db
 from core.apis import decorators
 from core.apis.responses import APIResponse
-from core.models.assignments import Assignment
-
-from .schema import AssignmentSchema, AssignmentSubmitSchema
-student_assignments_resources = Blueprint('student_assignments_resources', __name__)
-
-
-@student_assignments_resources.route('/assignments', methods=['GET'], strict_slashes=False)
-@decorators.authenticate_principal
-def list_assignments(p):
-    """Returns list of assignments"""
-    students_assignments = Assignment.get_assignments_by_student(p.student_id)
-    students_assignments_dump = AssignmentSchema().dump(students_assignments, many=True)
-    return APIResponse.respond(data=students_assignments_dump)
-
-
-@student_assignments_resources.route('/assignments', methods=['POST'], strict_slashes=False)
-@decorators.accept_payload
+@@ -22,6 +23,9 @@ def list_assignments(p):
 @decorators.authenticate_principal
 def upsert_assignment(p, incoming_payload):
     """Create or Edit an assignment"""
+    if 'content' not in incoming_payload or incoming_payload['content'] is None:
+        return jsonify({'error': 'Content cannot be null'}), 400
+
     assignment = AssignmentSchema().load(incoming_payload)
     assignment.student_id = p.student_id
 
-    upserted_assignment = Assignment.upsert(assignment)
-    db.session.commit()
-    upserted_assignment_dump = AssignmentSchema().dump(upserted_assignment)
-    return APIResponse.respond(data=upserted_assignment_dump)
-
-
-@student_assignments_resources.route('/assignments/submit', methods=['POST'], strict_slashes=False)
-@decorators.accept_payload
-@decorators.authenticate_principal
-def submit_assignment(p, incoming_payload):
+@@ -38,11 +42,24 @@ def submit_assignment(p, incoming_payload):
     """Submit an assignment"""
     submit_assignment_payload = AssignmentSubmitSchema().load(incoming_payload)
 
@@ -46,3 +25,24 @@ def submit_assignment(p, incoming_payload):
     db.session.commit()
     submitted_assignment_dump = AssignmentSchema().dump(submitted_assignment)
     return APIResponse.respond(data=submitted_assignment_dump)
+    try:
+        submitted_assignment = Assignment.submit(
+            _id=submit_assignment_payload.id,
+            teacher_id=submit_assignment_payload.teacher_id,
+            auth_principal=p
+        )
+        db.session.commit()
+        submitted_assignment_dump = AssignmentSchema().dump(submitted_assignment)
+        return APIResponse.respond(data=submitted_assignment_dump)
+
+    except ValueError as e:
+        return APIResponse.respond_with_error('FyleError', str(e), 400)
+
+    # submitted_assignment = Assignment.submit(
+    #     _id=submit_assignment_payload.id,
+    #     teacher_id=submit_assignment_payload.teacher_id,
+    #     auth_principal=p
+    # )
+    # db.session.commit()
+    # submitted_assignment_dump = AssignmentSchema().dump(submitted_assignment)
+    # return APIResponse.respond(data=submitted_assignment_dump)
