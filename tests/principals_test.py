ade9cd6a4e1bc281f247e777c1b16be8ec853ad2
3OOM <abhisheknikam369@gmail.com>
2024-09-21 18:33:56 +0530

    new file:   docker-compose.yml 	modified:   tests/SQL/count_grade_A_assignments_by_teacher_with_max_grading.sql 	modified:   tests/SQL/number_of_graded_assignments_for_each_student.sql 	new file:   tests/assertions_test.py 	new file:   tests/exceptions_test.py 	modified:   tests/principals_test.py 	new file:   tests/server_test.py 	modified:   tests/students_test.py 	modified:   tests/teachers_test.py

--- tests/principals_test.py
+++ tests/principals_test.py
@@ -13,6 +13,14 @@ def test_get_assignments(client, h_principal):
     for assignment in data:
         assert assignment['state'] in [AssignmentStateEnum.SUBMITTED, AssignmentStateEnum.GRADED]
 
+def test_get_teachers(client, h_principal):
+    response = client.get(
+        '/principal/teachers',
+        headers=h_principal
+    )
+
+    assert response.status_code == 200
+
 
 def test_grade_assignment_draft_assignment(client, h_principal):
     """
