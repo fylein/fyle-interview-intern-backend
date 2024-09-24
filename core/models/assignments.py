74aa287a4019406bc0121500ee887fb0dbd563d1
Ashwath Kannan <ash2k3@Ashwaths-MacBook-Air.local>
2024-09-24 17:58:54 +0530

    Fix unit tests - teachers_test.py

--- core/models/assignments.py
+++ core/models/assignments.py
@@ -79,6 +79,7 @@ class Assignment(db.Model):
         assignment = Assignment.get_by_id(_id)
         assertions.assert_found(assignment, 'No assignment with this id was found')
         assertions.assert_valid(grade is not None, 'assignment with empty grade cannot be graded')
+        assertions.assert_valid((assignment.teacher_id == auth_principal.teacher_id)|(auth_principal.principal_id is not None), 'This assignment belongs to some other teacher')
 
         assignment.grade = grade
         assignment.state = AssignmentStateEnum.GRADED
@@ -91,5 +92,5 @@ class Assignment(db.Model):
         return cls.filter(cls.student_id == student_id).all()
 
     @classmethod
-    def get_assignments_by_teacher(cls):
-        return cls.query.all()
+    def get_assignments_by_teacher(cls, teacher_id):
+        return cls.filter(cls.teacher_id == teacher_id).all()
