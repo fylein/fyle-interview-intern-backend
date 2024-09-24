80c0005a48d76a79ec3fbf0b45a822c619e9308d
Aaryan Patel <97274994+Aaryan33@users.noreply.github.com>
2024-09-25 00:20:11 +0530

    Task Completed

--- core/apis/assignments/student.py
+++ core/apis/assignments/student.py
@@ -2,11 +2,11 @@ from flask import Blueprint
 from core import db
 from core.apis import decorators
 from core.apis.responses import APIResponse
-from core.models.assignments import Assignment
-
+from core.models.assignments import Assignment, AssignmentStateEnum
+from core.libs import assertions
 from .schema import AssignmentSchema, AssignmentSubmitSchema
-student_assignments_resources = Blueprint('student_assignments_resources', __name__)
 
+student_assignments_resources = Blueprint('student_assignments_resources', __name__)
 
 @student_assignments_resources.route('/assignments', methods=['GET'], strict_slashes=False)
 @decorators.authenticate_principal
@@ -16,33 +16,36 @@ def list_assignments(p):
     students_assignments_dump = AssignmentSchema().dump(students_assignments, many=True)
     return APIResponse.respond(data=students_assignments_dump)
 
-
+# POST /student/assignments
 @student_assignments_resources.route('/assignments', methods=['POST'], strict_slashes=False)
 @decorators.accept_payload
 @decorators.authenticate_principal
 def upsert_assignment(p, incoming_payload):
     """Create or Edit an assignment"""
     assignment = AssignmentSchema().load(incoming_payload)
-    assignment.student_id = p.student_id
+    assignment.student_id = p.student_id        
+
+    assertions.assert_valid(assignment.content != None, "Assignment with null content cannot be submitted.")
 
     upserted_assignment = Assignment.upsert(assignment)
     db.session.commit()
     upserted_assignment_dump = AssignmentSchema().dump(upserted_assignment)
     return APIResponse.respond(data=upserted_assignment_dump)
 
-
+# POST /student/assignments/submit
 @student_assignments_resources.route('/assignments/submit', methods=['POST'], strict_slashes=False)
 @decorators.accept_payload
 @decorators.authenticate_principal
 def submit_assignment(p, incoming_payload):
     """Submit an assignment"""
     submit_assignment_payload = AssignmentSubmitSchema().load(incoming_payload)
-
+    
     submitted_assignment = Assignment.submit(
         _id=submit_assignment_payload.id,
         teacher_id=submit_assignment_payload.teacher_id,
         auth_principal=p
     )
+
     db.session.commit()
     submitted_assignment_dump = AssignmentSchema().dump(submitted_assignment)
     return APIResponse.respond(data=submitted_assignment_dump)
