98707aa8a0b8e2835d3cb5464aa3be3469c4d004
Aaryan Patel <97274994+Aaryan33@users.noreply.github.com>
2024-09-25 00:17:20 +0530

    Task Completed

--- core/apis/assignments/schema.py
+++ core/apis/assignments/schema.py
@@ -1,7 +1,7 @@
 from marshmallow import Schema, EXCLUDE, fields, post_load
 from marshmallow_sqlalchemy import SQLAlchemyAutoSchema, auto_field
 from marshmallow_enum import EnumField
-from core.models.assignments import Assignment, GradeEnum
+from core.models.assignments import Assignment, GradeEnum, Teacher
 from core.libs.helpers import GeneralObject
 
 
@@ -21,7 +21,7 @@ class AssignmentSchema(SQLAlchemyAutoSchema):
 
     @post_load
     def initiate_class(self, data_dict, many, partial):
-        # pylint: disable=unused-argument,no-self-use
+
         return Assignment(**data_dict)
 
 
@@ -34,7 +34,7 @@ class AssignmentSubmitSchema(Schema):
 
     @post_load
     def initiate_class(self, data_dict, many, partial):
-        # pylint: disable=unused-argument,no-self-use
+
         return GeneralObject(**data_dict)
 
 
@@ -47,5 +47,22 @@ class AssignmentGradeSchema(Schema):
 
     @post_load
     def initiate_class(self, data_dict, many, partial):
-        # pylint: disable=unused-argument,no-self-use
+        return GeneralObject(**data_dict)
+
+
+class TeacherSchema(SQLAlchemyAutoSchema):
+    
+    class Meta:
+
+        model = Teacher
+        unknown = EXCLUDE
+
+    id = auto_field(allow_none=False, required=True) # being consistent with the table attrs.
+    user_id = auto_field()
+
+    created_at = auto_field(dump_only=True)
+    updated_at = auto_field(dump_only=True)
+
+    @post_load
+    def initiate_class(self, data_dict, many, partial):
         return GeneralObject(**data_dict)
