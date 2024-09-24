30fae6bddd1421b6cc3f7f97ba496333bbfd02db
Aaryan Patel <97274994+Aaryan33@users.noreply.github.com>
2024-09-25 00:38:08 +0530

    Task Completed

--- tests/SQL/count_grade_A_assignments_by_teacher_with_max_grading.sql
+++ tests/SQL/count_grade_A_assignments_by_teacher_with_max_grading.sql
@@ -1,1 +1,18 @@
--- Write query to find the number of grade A's given by the teacher who has graded the most assignments
+-- -- Write query to find the number of grade A's given by the teacher who has graded the most assignments
+
+/*
+ constraints: 
+ > we cant use vars in sqlite.
+ > we cant nest queries. {sqlalchemy exception} tried creating a temp table and put the id of the mvp teacher in it but it didnt work.
+*/
+
+SELECT
+    COUNT(*) AS no_of_As
+FROM
+    assignments
+WHERE
+    grade == 'A'
+GROUP BY
+    teacher_id
+ORDER BY
+    no_of_As DESC;
