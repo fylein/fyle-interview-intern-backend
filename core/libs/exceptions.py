46b53f3983a8e61718d75df905d7c45081ccb2dd
Aaryan Patel <97274994+Aaryan33@users.noreply.github.com>
2024-09-25 00:27:00 +0530

    Task Completed

--- core/libs/exceptions.py
+++ core/libs/exceptions.py
@@ -5,7 +5,6 @@ class FyleError(Exception):
         Exception.__init__(self)
         self.message = message
         self.status_code = status_code
-
     def to_dict(self):
         res = dict()
         res['message'] = self.message
