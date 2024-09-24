e5a076c6db2b551451ec315ba7ad76db04b9f9e4
Aaryan Patel <97274994+Aaryan33@users.noreply.github.com>
2024-09-25 00:16:47 +0530

    Task Completed

--- core/apis/assignments/principal.py
+++ core/apis/assignments/principal.py
@@ -1,51 +1,49 @@
 from flask import Blueprint
-from core import db
 from core.apis import decorators
+from core.models.assignments import Assignment, Teacher
+from .schema import AssignmentSchema, AssignmentGradeSchema, TeacherSchema
 from core.apis.responses import APIResponse
-from core.models.assignments import Assignment, AssignmentStateEnum
-from core.libs import assertions
-from .schema import AssignmentSchema, AssignmentSubmitSchema
+from core import db
 
-student_assignments_resources = Blueprint('student_assignments_resources', __name__)
+principal_resources = Blueprint('principal_resources', __name__)
 
-@student_assignments_resources.route('/assignments', methods=['GET'], strict_slashes=False)
+# GET /principal/assignments
+@principal_resources.route("/assignments", methods=['GET'], strict_slashes=False)
 @decorators.authenticate_principal
 def list_assignments(p):
-    """Returns list of assignments"""
-    students_assignments = Assignment.get_assignments_by_student(p.student_id)
-    students_assignments_dump = AssignmentSchema().dump(students_assignments, many=True)
-    return APIResponse.respond(data=students_assignments_dump)
 
-# POST /student/assignments
-@student_assignments_resources.route('/assignments', methods=['POST'], strict_slashes=False)
-@decorators.accept_payload
+    """Returns list of graded or submitted assignments"""
+    submitted_or_graded_assignment = Assignment.get_submitted_or_graded_assignments(
+    )  # this is a python obj that we want to store persistently inside a db.
+
+    principal_assignments_dump = AssignmentSchema().dump(
+        submitted_or_graded_assignment, many=True)  # but before that we need to serialize it so that it could be sent over the wire.
+    return APIResponse.respond(data=principal_assignments_dump)
+
+# GET /principal/teachers
+@principal_resources.route("/teachers", methods=['GET'], strict_slashes=False)
 @decorators.authenticate_principal
-def upsert_assignment(p, incoming_payload):
-    """Create or Edit an assignment"""
-    assignment = AssignmentSchema().load(incoming_payload)
-    assignment.student_id = p.student_id        
-
-    assertions.assert_valid(assignment.content != None, "Assignment with null content cannot be submitted.")
-
-    upserted_assignment = Assignment.upsert(assignment)
-    db.session.commit()
-    upserted_assignment_dump = AssignmentSchema().dump(upserted_assignment)
-    return APIResponse.respond(data=upserted_assignment_dump)
-
-# POST /student/assignments/submit
-@student_assignments_resources.route('/assignments/submit', methods=['POST'], strict_slashes=False)
-@decorators.accept_payload
-@decorators.authenticate_principal
-def submit_assignment(p, incoming_payload):
-    """Submit an assignment"""
-    submit_assignment_payload = AssignmentSubmitSchema().load(incoming_payload)
+def get_teachers(p):
     
-    submitted_assignment = Assignment.submit(
-        _id=submit_assignment_payload.id,
-        teacher_id=submit_assignment_payload.teacher_id,
-        auth_principal=p
-    )
+    """ Returns a list of  the Teachers. """
+    teachers = Teacher.list_teachers()
+    teachers_dump = TeacherSchema().dump(teachers, many=True)
+    return APIResponse.respond(data=teachers_dump)
 
+
+# POST /principal/assignments/grade
+@principal_resources.route("/assignments/grade", methods=['POST'] , strict_slashes=False)
+@decorators.accept_payload
+@decorators.authenticate_principal
+def grade_assignments(p, incoming_payload):
+    assignment_to_grade_payload = AssignmentGradeSchema().load(incoming_payload)
+
+    graded_assignment = Assignment.regrade(
+        _id=assignment_to_grade_payload.id,
+        grade=assignment_to_grade_payload.grade
+        )
+    
     db.session.commit()
-    submitted_assignment_dump = AssignmentSchema().dump(submitted_assignment)
-    return APIResponse.respond(data=submitted_assignment_dump)
+
+    graded_assignment_dump = AssignmentSchema().dump(graded_assignment)
+    return APIResponse.respond(data=graded_assignment_dump)
