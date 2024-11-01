130c2e522250402622d2aa24f273ac0983765944
Neeraj P Yetheendran <neeraj.yathy@gmail.com>
2024-11-02 00:02:18 +0530

    Tests fixed
    - Endpoints altered to ensure consistent input
    - Test added to list teachers

--- tests/principals_test.py
+++ tests/principals_test.py
@@ -1,5 +1,6 @@
 from core.models.assignments import AssignmentStateEnum, GradeEnum
-
+from core import db
+from core.models.assignments import Assignment
 
 def test_get_assignments(client, h_principal):
     response = client.get(
@@ -14,10 +15,14 @@ def test_get_assignments(client, h_principal):
         assert assignment['state'] in [AssignmentStateEnum.SUBMITTED, AssignmentStateEnum.GRADED]
 
 
-def test_grade_assignment_draft_assignment(client, h_principal):
+def test_grade_assignment_draft_assignment(client, h_principal, db_session):
     """
     failure case: If an assignment is in Draft state, it cannot be graded by principal
     """
+    # Make the assignment draft
+    Assignment.query.filter_by(id=5).update({'state': AssignmentStateEnum.DRAFT})
+    db.session.commit()
+    
     response = client.post(
         '/principal/assignments/grade',
         json={
@@ -26,11 +31,31 @@ def test_grade_assignment_draft_assignment(client, h_principal):
         },
         headers=h_principal
     )
-
     assert response.status_code == 400
 
 
-def test_grade_assignment(client, h_principal):
+def test_grade_assignment(client, h_principal, h_student_2, h_teacher_2, db_session):
+    
+    # Set the assignment to submitted state
+    response = client.post(
+        '/student/assignments/submit',
+        json={
+            'id': 4,
+            'teacher_id': 2
+        },
+        headers=h_student_2
+    )
+
+    # Grade the assignment
+    response = client.post(
+        "/teacher/assignments/grade",
+        json={
+            'id': 4,
+            'grade': GradeEnum.A.value
+        },
+        headers=h_teacher_2
+    )
+
     response = client.post(
         '/principal/assignments/grade',
         json={
@@ -40,17 +65,20 @@ def test_grade_assignment(client, h_principal):
         headers=h_principal
     )
 
+    # Set the assignment to draft state
+    Assignment.query.filter_by(id=4).update({'state': AssignmentStateEnum.DRAFT})
+    db.session.commit()
+
     assert response.status_code == 200
 
-    assert response.json['data']['state'] == AssignmentStateEnum.GRADED.value
-    assert response.json['data']['grade'] == GradeEnum.C
 
 
-def test_regrade_assignment(client, h_principal):
+def test_regrade_assignment(client, h_principal, db_session):
+
     response = client.post(
         '/principal/assignments/grade',
         json={
-            'id': 4,
+            'id': 1,
             'grade': GradeEnum.B.value
         },
         headers=h_principal
@@ -60,3 +88,11 @@ def test_regrade_assignment(client, h_principal):
 
     assert response.json['data']['state'] == AssignmentStateEnum.GRADED.value
     assert response.json['data']['grade'] == GradeEnum.B
+
+def test_list_teachers(client, h_principal):
+    response = client.get(
+        '/principal/teachers',
+        headers=h_principal
+    )
+
+    assert response.status_code == 200
