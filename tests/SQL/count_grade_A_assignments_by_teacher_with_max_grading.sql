34d68534471f5a992ca8759b8fa48b18b835d6ff
Neeraj P Yetheendran <neeraj.yathy@gmail.com>
2024-11-02 00:27:59 +0530

    query added to find the number of grade A's given by the teacher who has graded the most assignments

--- tests/SQL/count_grade_A_assignments_by_teacher_with_max_grading.sql
+++ tests/SQL/count_grade_A_assignments_by_teacher_with_max_grading.sql
@@ -1,1 +1,35 @@
 -- Write query to find the number of grade A's given by the teacher who has graded the most assignments
+WITH TotalGrades AS (
+    SELECT 
+        teacher_id, 
+        COUNT(*) AS total_count
+    FROM 
+        assignments
+    GROUP BY 
+        teacher_id
+),
+AGradeCount AS (
+    SELECT 
+        teacher_id, 
+        COUNT(*) AS a_grade_count
+    FROM 
+        assignments
+    WHERE 
+        grade = 'A'
+    GROUP BY 
+        teacher_id
+)
+SELECT 
+    agc.a_grade_count
+FROM 
+    TotalGrades tg
+JOIN 
+    AGradeCount agc 
+    ON tg.teacher_id = agc.teacher_id
+WHERE 
+    tg.total_count = (
+        SELECT 
+            MAX(total_count) 
+        FROM 
+            TotalGrades
+    );
