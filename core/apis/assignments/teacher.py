64aa9bd638efaa70ef60f90ddbeefd22e630b790
Neeraj P Yetheendran <neeraj.yathy@gmail.com>
2024-11-01 23:37:43 +0530

    Teacher Endpoints Fixed
    - Arguments passed to decorators modified to reflect change in blueprint
    - try except block added around request body

--- core/apis/assignments/teacher.py
+++ core/apis/assignments/teacher.py
@@ -3,32 +3,44 @@ from core import db
 from core.apis import decorators
 from core.apis.responses import APIResponse
 from core.models.assignments import Assignment
+from core.libs.exceptions import FyleError
+from flask import request
 
 from .schema import AssignmentSchema, AssignmentGradeSchema
 teacher_assignments_resources = Blueprint('teacher_assignments_resources', __name__)
 
 
-@teacher_assignments_resources.route('/assignments', methods=['GET'], strict_slashes=False)
+@teacher_assignments_resources.route('/', methods=['GET'], strict_slashes=False)
 @decorators.authenticate_principal
 def list_assignments(p):
     """Returns list of assignments"""
-    teachers_assignments = Assignment.get_assignments_by_teacher()
+
+    teachers_assignments = Assignment.get_assignments_by_teacher(p.teacher_id)
     teachers_assignments_dump = AssignmentSchema().dump(teachers_assignments, many=True)
     return APIResponse.respond(data=teachers_assignments_dump)
 
 
-@teacher_assignments_resources.route('/assignments/grade', methods=['POST'], strict_slashes=False)
+@teacher_assignments_resources.route('/grade', methods=['POST'], strict_slashes=False)
 @decorators.accept_payload
 @decorators.authenticate_principal
 def grade_assignment(p, incoming_payload):
     """Grade an assignment"""
     grade_assignment_payload = AssignmentGradeSchema().load(incoming_payload)
 
-    graded_assignment = Assignment.mark_grade(
-        _id=grade_assignment_payload.id,
-        grade=grade_assignment_payload.grade,
-        auth_principal=p
-    )
-    db.session.commit()
-    graded_assignment_dump = AssignmentSchema().dump(graded_assignment)
+    try:
+        graded_assignment = Assignment.mark_grade(
+            _id=grade_assignment_payload.id,
+            grade=grade_assignment_payload.grade,
+            auth_principal=p # Is this the correct parameter?
+        )
+        db.session.commit()
+        graded_assignment_dump = AssignmentSchema().dump(graded_assignment)
+    
+    except FyleError as e:
+        db.session.rollback()
+        raise e
+    except Exception as e:
+        db.session.rollback()
+        raise FyleError(message=str(e), status_code=400)
+    
     return APIResponse.respond(data=graded_assignment_dump)
