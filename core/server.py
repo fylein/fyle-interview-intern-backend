311ffe7221e5ba6fc5a16f3f4e2b9ee13c68c0e8
Neeraj P Yetheendran <neeraj.yathy@gmail.com>
2024-11-01 23:28:04 +0530

    Blueprints registered for endpoints
    Blueprints registerd for the following endpoints:
    student assignment endpoints
    teacher assignment endpoints
    principal assignment endpoints
    principal teacher endpoints

--- core/server.py
+++ core/server.py
@@ -1,16 +1,18 @@
 from flask import jsonify
 from marshmallow.exceptions import ValidationError
 from core import app
-from core.apis.assignments import student_assignments_resources, teacher_assignments_resources
+from core.apis.assignments import student_assignments_resources, teacher_assignments_resources, principal_assignments_resources
+from core.apis.teachers import principal_teacher_resources
 from core.libs import helpers
 from core.libs.exceptions import FyleError
 from werkzeug.exceptions import HTTPException
 
 from sqlalchemy.exc import IntegrityError
 
-app.register_blueprint(student_assignments_resources, url_prefix='/student')
-app.register_blueprint(teacher_assignments_resources, url_prefix='/teacher')
-
+app.register_blueprint(student_assignments_resources, url_prefix='/student/assignments')
+app.register_blueprint(teacher_assignments_resources, url_prefix='/teacher/assignments')
+app.register_blueprint(principal_assignments_resources, url_prefix='/principal/assignments')
+app.register_blueprint(principal_teacher_resources, url_prefix='/principal/teachers')
 
 @app.route('/')
 def ready():
