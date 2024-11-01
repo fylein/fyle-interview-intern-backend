f229e3aaa9d9f86a5af12e8b718d8736a7218eaf
Neeraj P Yetheendran <neeraj.yathy@gmail.com>
2024-11-01 23:45:39 +0530

    Assignment model modified
    - New Assertions added for additional validation
    - try and except blocks added around method body
    - re_grade method added for principal to regrade graded assignments

--- core/models/assignments.py
+++ core/models/assignments.py
@@ -5,6 +5,7 @@ from core.libs import helpers, assertions
 from core.models.teachers import Teacher
 from core.models.students import Student
 from sqlalchemy.types import Enum as BaseEnum
+from core.libs.exceptions import FyleError
 
 
 class GradeEnum(str, enum.Enum):
@@ -39,8 +40,10 @@ class Assignment(db.Model):
         db_query = db.session.query(cls)
         return db_query.filter(*criterion)
 
+
     @classmethod
     def get_by_id(cls, _id):
+
         return cls.filter(cls.id == _id).first()
 
     @classmethod
@@ -65,8 +68,9 @@ class Assignment(db.Model):
         assertions.assert_found(assignment, 'No assignment with this id was found')
         assertions.assert_valid(assignment.student_id == auth_principal.student_id, 'This assignment belongs to some other student')
         assertions.assert_valid(assignment.content is not None, 'assignment with empty content cannot be submitted')
+        assertions.assert_valid(assignment.state == AssignmentStateEnum.DRAFT, 'only a draft assignment can be submitted')
 
-        assignment.teacher_id = teacher_id
+        assignment.state = AssignmentStateEnum.SUBMITTED
         db.session.flush()
 
         return assignment
@@ -74,20 +78,49 @@ class Assignment(db.Model):
 
     @classmethod
     def mark_grade(cls, _id, grade, auth_principal: AuthPrincipal):
-        assignment = Assignment.get_by_id(_id)
-        assertions.assert_found(assignment, 'No assignment with this id was found')
-        assertions.assert_valid(grade is not None, 'assignment with empty grade cannot be graded')
+        
+        try:
+            assignment = Assignment.get_by_id(_id)
+            assertions.assert_found(assignment, 'No assignment with this id was found')
+            assertions.assert_valid(grade is not None, 'assignment with empty grade cannot be graded')
+            assertions.assert_valid(assignment.teacher_id == auth_principal.teacher_id, 'This assignment belongs to some other teacher')
 
-        assignment.grade = grade
-        assignment.state = AssignmentStateEnum.GRADED
-        db.session.flush()
+            assignment.grade = grade
+            assignment.state = AssignmentStateEnum.GRADED
+            db.session.flush()
 
-        return assignment
+            return assignment
+        except FyleError as e:
+            db.session.rollback()
+            raise e
+        except Exception as e:
+            db.session.rollback()
+            raise FyleError(message=str(e), status_code=400)
+
 
     @classmethod
     def get_assignments_by_student(cls, student_id):
         return cls.filter(cls.student_id == student_id).all()
 
     @classmethod
-    def get_assignments_by_teacher(cls):
-        return cls.query.all()
+    def get_assignments_by_teacher(cls, teacher_id):
+        return cls.filter(cls.teacher_id == teacher_id).all()
+        
+    @classmethod
+    def re_grade(cls, _id, grade, auth_principal: AuthPrincipal):
+
+        try:
+
+            assignment = Assignment.get_by_id(_id)
+            assertions.assert_found(assignment, 'No assignment with this id was found')
+            print("auth_principal",auth_principal.principal_id)
+            print("assignment.state",assignment.state)
+            if not auth_principal.principal_id:
+                assertions.assert_valid(assignment.teacher_id == auth_principal.teacher_id, 'This assignment belongs to some other teacher')
+            assertions.assert_valid(assignment.state == AssignmentStateEnum.GRADED, 'only graded assignment can be regraded')
+
+            assignment.grade = grade
+            return assignment
+        
+        except Exception as e:
+            raise e
