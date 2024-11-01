d09d3bc533942934e3caf4e657d1413bcc3f2064
Neeraj P Yetheendran <neeraj.yathy@gmail.com>
2024-11-01 23:35:42 +0530

    Fixed Student Endpoints
    - Arguments passed to decorators modified to reflect change in blueprint
    - try except block added around endpoint body
    - assertions used for endpoint validation

--- core/apis/assignments/student.py
+++ core/apis/assignments/student.py
@@ -3,21 +3,26 @@ from core import db
 from core.apis import decorators
 from core.apis.responses import APIResponse
 from core.models.assignments import Assignment
-
+from core.libs import assertions
+from core.libs.exceptions import FyleError
+from flask import request
 from .schema import AssignmentSchema, AssignmentSubmitSchema
+from core.models.assignments import AssignmentStateEnum
+
 student_assignments_resources = Blueprint('student_assignments_resources', __name__)
 
 
-@student_assignments_resources.route('/assignments', methods=['GET'], strict_slashes=False)
+@student_assignments_resources.route('/', methods=['GET'], strict_slashes=False)
 @decorators.authenticate_principal
 def list_assignments(p):
     """Returns list of assignments"""
+
     students_assignments = Assignment.get_assignments_by_student(p.student_id)
     students_assignments_dump = AssignmentSchema().dump(students_assignments, many=True)
     return APIResponse.respond(data=students_assignments_dump)
 
 
-@student_assignments_resources.route('/assignments', methods=['POST'], strict_slashes=False)
+@student_assignments_resources.route('/', methods=['POST'], strict_slashes=False)
 @decorators.accept_payload
 @decorators.authenticate_principal
 def upsert_assignment(p, incoming_payload):
@@ -25,24 +30,53 @@ def upsert_assignment(p, incoming_payload):
     assignment = AssignmentSchema().load(incoming_payload)
     assignment.student_id = p.student_id
 
-    upserted_assignment = Assignment.upsert(assignment)
-    db.session.commit()
-    upserted_assignment_dump = AssignmentSchema().dump(upserted_assignment)
+    try:
+        # Check if content is not empty
+        if not request.json.get('content'):
+            raise FyleError(message='Content cannot be empty', status_code=400)
+
+        upserted_assignment = Assignment.upsert(assignment)
+        db.session.commit()
+        upserted_assignment_dump = AssignmentSchema().dump(upserted_assignment)
+
+    except Exception as e:
+        db.session.rollback()
+        raise FyleError(message=str(e), status_code=400)
+    
     return APIResponse.respond(data=upserted_assignment_dump)
 
 
-@student_assignments_resources.route('/assignments/submit', methods=['POST'], strict_slashes=False)
+@student_assignments_resources.route('/submit', methods=['POST'], strict_slashes=False)
 @decorators.accept_payload
 @decorators.authenticate_principal
 def submit_assignment(p, incoming_payload):
     """Submit an assignment"""
     submit_assignment_payload = AssignmentSubmitSchema().load(incoming_payload)
 
-    submitted_assignment = Assignment.submit(
-        _id=submit_assignment_payload.id,
-        teacher_id=submit_assignment_payload.teacher_id,
-        auth_principal=p
-    )
-    db.session.commit()
-    submitted_assignment_dump = AssignmentSchema().dump(submitted_assignment)
+    try:
+
+        assignment = Assignment.get_by_id(submit_assignment_payload.id)
+        assertions.assert_valid(assignment.state == AssignmentStateEnum.DRAFT, 'only a draft assignment can be submitted')
+        assertions.assert_valid(assignment.student_id == p.student_id, 'This assignment belongs to some other student')
+        assertions.assert_valid(assignment.teacher_id == submit_assignment_payload.teacher_id, 'This assignment belongs to some other teacher')
+        assertions.assert_valid(assignment.content is not None, 'assignment with empty content cannot be submitted')
+       
+        submitted_assignment = Assignment.submit(
+            _id=submit_assignment_payload.id,
+            teacher_id=submit_assignment_payload.teacher_id,
+            auth_principal=p
+        )
+        db.session.commit()
+        submitted_assignment_dump = AssignmentSchema().dump(submitted_assignment)
+
+    except FyleError as e:
+        print("e",str(e))
+        db.session.rollback()
+        raise e
+    
+    except Exception as e:
+        db.session.rollback()
+        print("E",str(e))
+        raise FyleError(message=str(e)[:-1], status_code=400)
+    
     return APIResponse.respond(data=submitted_assignment_dump)
