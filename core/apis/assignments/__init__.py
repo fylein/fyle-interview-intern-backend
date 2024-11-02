a81fcbc05bf658dc070ad8d1e6f3a0ee90bc7aeb
Neeraj P Yetheendran <neeraj.yathy@gmail.com>
2024-11-02 17:17:55 +0530

    Reformatted files using Black Formatter

--- core/apis/assignments/__init__.py
+++ core/apis/assignments/__init__.py
@@ -1,3 +1,5 @@
 from .student import student_assignments_resources as student_assignments_resources
 from .teacher import teacher_assignments_resources as teacher_assignments_resources
-from .principal import principal_assignments_resources as principal_assignments_resources
+from .principal import (
+    principal_assignments_resources as principal_assignments_resources,
+)
