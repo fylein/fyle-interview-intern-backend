43a4cb44a2157813ec3fa4ef1d56509e79e3ceca
Neeraj P Yetheendran <neeraj.yathy@gmail.com>
2024-11-02 12:24:46 +0530

    Test added for user model methods

---
+++ tests/user_test.py
@@ -0,0 +1,30 @@
+from core.models.users import User
+from core import db
+from sqlalchemy import text
+import datetime
+
+
+def test_create_user():
+    user = User(id=90,username='test256',email='test256@gmail.com',created_at=datetime.datetime.now(),updated_at=datetime.datetime.now())
+    db.session.add(user)
+    db.session.commit()
+    assert user.id == 90
+    assert user.username == 'test256'
+    assert user.email == 'test256@gmail.com'
+
+    # Remove the user from the database
+    db.session.delete(user)
+    db.session.commit()
+
+def test_filter_user():
+    criterion = (User.id == 1)
+    user = User.filter(criterion).first()
+    assert user.id == 1
+
+def test_get_user_by_id():
+    user = User.get_by_id(1)
+    assert user.id == 1
+
+def test_get_user_by_email():
+    user = User.get_by_email('student1@fylebe.com')
+    assert user.id == 1
