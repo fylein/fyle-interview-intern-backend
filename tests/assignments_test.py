722f923de49cd3ae6669084b3d82015190dc312d
Neeraj P Yetheendran <neeraj.yathy@gmail.com>
2024-11-02 16:51:27 +0530

    Assignment Tests Added
    Tests added for:
    
    - Checking handling of FyleError raise inside upsert() method
    - Checking handling of other errors raise inside upsert() method
    - Checking handling of FyleError raise inside mark_grade() method
    - Checking handling of other errors inside mark_grade() method
    - Checking handling of regrading of assignment belonging to other teacher

---
+++ tests/assignments_test.py
@@ -0,0 +1,31 @@
+from core.models.assignments import Assignment, AssignmentStateEnum
+from core.libs.exceptions import FyleError
+import pytest
+from core.apis.decorators import AuthPrincipal
+
+def test_upsert_assignment_fyle_error():
+    with pytest.raises(FyleError):
+        Assignment.upsert(Assignment(id=99, content='test', state=AssignmentStateEnum.SUBMITTED))
+
+def test_upsert_assignment_bad_input():
+    try:
+        Assignment.upsert('test')
+    except FyleError as e:
+        assert e.message == "'str' object has no attribute 'id'"
+
+def test_mark_grade_fyle_error():
+    with pytest.raises(FyleError):
+        Assignment.mark_grade(99, 100, None)
+
+def test_mark_grade_bad_input():
+    try:
+        Assignment.mark_grade(1, 1, None)
+    except FyleError as e:
+        assert e.message == "'NoneType' object has no attribute 'teacher_id'"
+        
+def test_re_grade_belongs_to_other_teacher():
+    principal = AuthPrincipal(teacher_id=1, user_id=1)
+    try:
+        Assignment.re_grade(1, "A",principal)
+    except FyleError as e:
+        assert e.message == 'This assignment belongs to some other teacher'
